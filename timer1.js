73a6aeaf700166480a9269dca3a44aa20a10ab34
Manuel Casanova <manucasanova@hotmail.com>
2022-02-17 07:54:15 -0800

    code working I cannot add a line

--- timer1.js
+++ timer1.js
@@ -1,20 +1,32 @@
 // Implement an alarm clock / timer which will beep after a specified amount of time has passed. The user can specify an unlimited number of alarms using command line arguments
 
-
-
-
-
-// setTimeout(() => {
-//   process.stdout.write("\n");
-// }, delay);
-
 let args = process.argv.slice(2)
+// let numOfArgs = args.length; 
+// let lastOfArgs = args[args.length - 1]; //Helps me to find the last item written in the terminal
 
 for(const arg of args){
   if (!isNaN(arg) && arg >= 0) { 
-    //!isNan -> is not not a number (therefore, is a number and it's a natural number)
+    //!isNan -> is not not a number (therefore, is a number) and it's a natural number)
     setTimeout(() => {
-      process.stdout.write(`ding`); //process.stdout.write('\x07') I cannot hear the sound
+      process.stdout.write(`ding `); //process.stdout.write('\x07') I cannot hear the sound
     }, arg * 1000);
   }
+  // setTimeout(() => {               //I want to fix the new line at the end but...when!?
+  //   process.stdout.write("\n");
+  // }, 0);
 }
+
+/*
+Alternative suggested, but uses arrow function and forEach loop and I feel I control more the code above
+*/
+
+// process.argv.slice(2).forEach(arg => {
+//   if (!isNaN(arg) && arg >= 0) { 
+//     //!isNan -> is not not a number (therefore, is a number and it's a natural number)
+//     setTimeout(() => {
+//       process.stdout.write(`*`); //process.stdout.write('\x07') I cannot hear the sound
+//     }, arg * 1000);
+//   }
+// });
+
+
