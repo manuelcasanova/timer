91f72d2eb8e9a70bcbbee78296281ff31c86cf31
Manuel Casanova <manucasanova@hotmail.com>
2022-02-16 18:36:57 -0800

    function working

--- timer1.js
+++ timer1.js
@@ -1,2 +1,20 @@
 // Implement an alarm clock / timer which will beep after a specified amount of time has passed. The user can specify an unlimited number of alarms using command line arguments
 
+
+
+
+
+// setTimeout(() => {
+//   process.stdout.write("\n");
+// }, delay);
+
+let args = process.argv.slice(2)
+
+for(const arg of args){
+  if (!isNaN(arg) && arg >= 0) { 
+    //!isNan -> is not not a number (therefore, is a number and it's a natural number)
+    setTimeout(() => {
+      process.stdout.write(`ding`); //process.stdout.write('\x07') I cannot hear the sound
+    }, arg * 1000);
+  }
+}
