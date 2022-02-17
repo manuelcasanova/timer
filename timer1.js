0c9a811fee300e727e8b3d51b0547b200c5c8042
Manuel Casanova <manucasanova@hotmail.com>
2022-02-17 09:19:43 -0800

    new line added successfully without spaces

--- timer1.js
+++ timer1.js
@@ -5,20 +5,24 @@ let numOfArgs = args.length;
 let lastOfArgs = args[args.length - 1]; //Helps me to find the last item written in the terminal
 
 
+let delay = 1000
+
 for(const arg of args){
-  let delay = 1000
+  
   if (!isNaN(arg) && arg >= 0) { 
-    //!isNan -> is not not a number (therefore, is a number) and it's a natural number)
+    //!isNan -> is not "not a number" (therefore, is a number) and it's a natural number)
     setTimeout(() => {
       process.stdout.write(`ding `); //process.stdout.write('\x07') I cannot hear the sound
     }, arg * delay);
   }
-    setTimeout(() => {               
-    process.stdout.write("\n");
-  }, lastOfArgs * delay + 1); //+1 so the new line happens after the last arg
 }
+setTimeout(() => {               
+  process.stdout.write("\n");
+}, lastOfArgs * delay + 1); //+1 so the new line happens after the last arg
 
-//I don't know why it prints everything properly, even the new line, but leavces three spaces
+//I don't know why it prints everything properly, even the new line, but leaves three spaces
+
+
 
 /*
 Alternative suggested, but uses arrow function and forEach loop and I feel I control more the code above
