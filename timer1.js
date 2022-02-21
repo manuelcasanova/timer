fa09a3f0c28acf1f8786377f5e914223aa7f8655
Manuel Casanova <manucasanova@hotmail.com>
2022-02-21 07:56:20 -0800

    REMOVED AN UNUSED VARIABLE, ADDED A CODE THAT RETURNS THE SOUND ON THE TERMINAL

--- timer1.js
+++ timer1.js
@@ -1,11 +1,9 @@
 // Implement an alarm clock / timer which will beep after a specified amount of time has passed. The user can specify an unlimited number of alarms using command line arguments
 
 let args = process.argv.slice(2)
-let numOfArgs = args.length; 
-let lastOfArgs = args[args.length - 1]; //Helps me to find the last item written in the terminal
+let lastOfArgs = args[args.length - 1]; //Helps me to find the last item written in the terminal which I will use to write the new line
 
-
-let delay = 1000
+let delay = 1000 //I decided to declare this variable so I can easily change the time without modifying the function
 
 for(const arg of args){
   
@@ -20,7 +18,7 @@ setTimeout(() => {
   process.stdout.write("\n");
 }, lastOfArgs * delay + 1); //+1 so the new line happens after the last arg
 
-//I don't know why it prints everything properly, even the new line, but leaves three spaces
+
 
 
 
@@ -37,4 +35,20 @@ Alternative suggested, but uses arrow function and forEach loop and I feel I con
 //   }
 // });
 
+//The code below is like the first one, but makes the sound on the terminal
 
+// let lastOfArgs2 = args[args.length - 1]; //Helps me to find the last item written in the terminal which I will use to write the new line
+
+// let delay2 = 1000 //I decided to declare this variable so I can easily change the time without modifying the function
+
+// for(const arg of args){
+  
+//   if (!isNaN(arg) && arg >= 0) { 
+//     setTimeout(() => {
+//       process.stdout.write('\x07'); //The sound plays on the terminal, not on VSCODE
+//     }, arg * delay);
+//   }
+// }
+// setTimeout(() => {               
+//   process.stdout.write("\n");
+// }, lastOfArgs2 * delay2 + 1);
